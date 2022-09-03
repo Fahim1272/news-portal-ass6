f095086c005f3658a3db48d3dc85b7278dc494da
Fahim1272 <imfahim72@gmail.com>
2022-09-04 04:33:27 +0600

    navbar added

--- js/app.js
+++ js/app.js
@@ -25,3 +25,67 @@ const showNews = async idCategory =>{
     }
     
 }
+const newsShow = data =>{
+   
+    const errorMessageNews = document.getElementById('errorMessage');
+    const newsItem = document.getElementById('news');
+
+    if(data.length == 0){
+        document.getElementById('newCount').classList.add('d-none');
+        errorMessageNews.classList.remove('d-none')
+        newsItem.textContent = "";
+        const LoadSpinner = document.getElementById('LoadSpinners');
+        LoadSpinner.classList.add('d-none');
+    }
+    else{
+        console.log(data.length)
+        document.getElementById('counter').innerText = data.length;
+        document.getElementById('newCount').classList.remove('d-none');
+        errorMessageNews.classList.add('d-none')
+        newsItem.textContent = "";
+        data.forEach(newsElements => {
+        
+        
+        const details = newsElements.details;
+        const detailsShort = details.substring(0,350) + '...';
+        const newsCard = document.createElement('div')
+        newsCard.innerHTML = `
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-2 d-flex justify-content-lg-start  justify-content-md-start justify-content-center">
+                        <img src="${newsElements.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-10 d-flex flex-column justify-content-center p-2" >
+                        <div class="card-body">
+                            <h5 class="card-title">${newsElements.title}</h5>
+                            <p class="card-text">${detailsShort}</p>
+                        </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex flex-column flex-sm-row flex-lg-row align-items-center me-1">
+                                <img class="writerProfilePIc ms-4" src="${newsElements.author.img}" alt="" />
+                                <div class="ms-2">
+                                    <h5>${newsElements.author.name ? newsElements.author.name : "No data"}</h5>
+                                    <p>${newsElements.author.published_date}</p>
+                                </div>
+                            </div>
+                            <div class="d-flex flex-column flex-sm-row flex-lg-row align-items-center me-1">
+                                <i class="fa-solid fa-eye"></i>
+                                <h4 class="ms-2">${newsElements.total_view ? newsElements.total_view : "no data"}</h4>
+                            </div>
+                            <div>
+                                <button onclick="showDetails('${newsElements._id}');" style="font-size: 24px; color: blue" class="btn btn-primary text-light p-1" data-bs-toggle="modal" data-bs-target="#newsModal">Read...</button>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+
+        ` // Spinner end
+        const LoadSpinner = document.getElementById('LoadSpinners');
+        LoadSpinner.classList.add('d-none');
+        newsItem.appendChild(newsCard)
+    })
+    }   
+}
